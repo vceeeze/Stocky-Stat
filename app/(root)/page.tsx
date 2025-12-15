aa92989682eee7f452e84c40b49ace5b0612e4a3
vceeeze <vceeeze11@gmail.com>
2025-12-15 18:23:57 +0100

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+
 
 const Home = () => {
   return (
-    <div>
-      <Button variant={'secondary'} className='' >REACT jsx </Button>
+    <div className='flex min-h-screen home-wrapper'>
+     HOME
     </div>
   )
 }
