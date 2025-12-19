00bcde19564d519332bf25b2c345a0ce555b5974
vceeeze <vceeeze11@gmail.com>
2025-12-19 09:12:55 +0100

    Implement database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+    }
+    
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if(!MONGODB_URI) throw new Error("MONGODB_URI must be defined in env variables");
+
+    if (cached.conn) return cached.conn;
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {bufferCommands: false})
+    }   
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        cached.promise = null;
+        throw err;
+    }
+
+
+    console.log(`MongoDB connected ${process.env.NODE_ENV} - ${MONGODB_URI}`  );
+
+}
